360bb156d51b2e247822ddb60b7e7a2b9a9216ef
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-11-21 22:52:53 -0800

    modified admin settings

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+R
+"asts_fitness_program/unit/admin.py,4\1\416b14d2c82d4fb043ae05d6a3a84f55a601f345
+W
+'asts_fitness_program/config/settings.py,2\6\268980cdad70cfb0c803a2f95b10ccb3b5c35c05
+S
+#asts_fitness_program/unit/models.py,9\3\9360148a28df88fe20c3c39bfc6414adc0cb4266
--- asts_fitness_program/unit/admin.py
+++ asts_fitness_program/unit/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
-from .models import fitness_level, airmen, physical_training_leader, profile, failure, unit_fitness_program_manager
+from .models import Fitness_Level, Airman, Physical_Training_Leader, Profile, Failure, Unit_Fitness_Program_Manager
+
 
 # Register your models here.
-admin.site.register(fitness_level)
-admin.site.register(airmen)
-admin.site.register(physical_training_leader)
-admin.site.register(profile)
-admin.site.register(failure)
-admin.site.register(unit_fitness_program_manager)
+
+# admin.site.register(Fitness_Level)
+# admin.site.register(Airman)
+# admin.site.register(Physical_Training_Leader)
+# admin.site.register(Profile)
+# admin.site.register(Failure)
+# admin.site.register(Unit_Fitness_Program_Manager)
+
+@admin.register(Fitness_Level)
+class FitnessLevelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Airman)
+class AirmanAdmin(admin.ModelAdmin):
+    list_display = ('rank', 'first_name', 'middle_initial', 'last_name', 'test_date')
+    list_filter = ('first_name', 'last_name', 'test_date')
+    search_fields = ('first_name', 'last_name')
+    prepopulated_fields = {'slug': ('ssn',)}
+    date_hierarchy = 'test_date'
+    ordering = ('test_date', 'last_name')
+
+# @admin.register(Physical_Training_Leader)
+# @admin.register(Profile)
+# @admin.register(Failure)
+# @admin.register(Unit_Fitness_Program_Manager)
