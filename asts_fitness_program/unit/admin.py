047a00d2dc31f4dfe5f1cac4997fb15b26414044
Ronald (Jeff) Lopez <lopez.j.ronald@gmail.com>
2020-11-22 01:12:13 -0800

    removed fitness levels and created is as a choice

--- asts_fitness_program/unit/admin.py
+++ asts_fitness_program/unit/admin.py
@@ -1,31 +1,48 @@
 from django.contrib import admin
-from .models import Fitness_Level, Airman, Physical_Training_Leader, Profile, Failure, Unit_Fitness_Program_Manager
+from .models import Airman, Physical_Training_Leader, Profile, Failure, Unit_Fitness_Program_Manager
 
 
 # Register your models here.
-
-# admin.site.register(Fitness_Level)
 # admin.site.register(Airman)
-# admin.site.register(Physical_Training_Leader)
-# admin.site.register(Profile)
-# admin.site.register(Failure)
-# admin.site.register(Unit_Fitness_Program_Manager)
-
-@admin.register(Fitness_Level)
-class FitnessLevelAdmin(admin.ModelAdmin):
-    pass
-
-
 @admin.register(Airman)
 class AirmanAdmin(admin.ModelAdmin):
     list_display = ('rank', 'first_name', 'middle_initial', 'last_name', 'test_date')
     list_filter = ('first_name', 'last_name', 'test_date')
     search_fields = ('first_name', 'last_name')
-    prepopulated_fields = {'slug': ('ssn',)}
+    prepopulated_fields = {'airman_slug': ('ssn',)}
     date_hierarchy = 'test_date'
     ordering = ('test_date', 'last_name')
 
+
 # @admin.register(Physical_Training_Leader)
-# @admin.register(Profile)
-# @admin.register(Failure)
-# @admin.register(Unit_Fitness_Program_Manager)
+@admin.register(Physical_Training_Leader)
+class PhysicalTrainingLeader(admin.ModelAdmin):
+    list_display = ('airman_id', 'ptl_certification_date', 'ptl_expiration_date', 'cpr_expiration_date')
+    list_filter = ('ptl_expiration_date', 'cpr_expiration_date')
+    date_hierarchy = 'ptl_expiration_date'
+    ordering = ('-ptl_expiration_date', '-cpr_expiration_date')
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('airman_id', 'profile_start_date', 'profile_expiration_date',)
+    list_filter = ('profile_expiration_date',)
+    date_hierarchy = 'profile_expiration_date'
+    ordering = ('-profile_expiration_date',)
+
+
+@admin.register(Failure)
+class FailureAdmin(admin.ModelAdmin):
+    list_display = ('airman_id', 'failure_date', 'be_well_completion_date',)
+    list_filter = ('failure_date',)
+    search_fields = ('failure_date',)
+    date_hierarchy = 'failure_date'
+    ordering = ('-failure_date',)
+
+
+@admin.register(Unit_Fitness_Program_Manager)
+class UnitFitnessProgramManagerAdmin(admin.ModelAdmin):
+    list_display = ('airman_id', 'ufpm_expiration_date')
+    list_filter = ('ufpm_expiration_date',)
+    date_hierarchy = 'ufpm_expiration_date'
+    ordering = ('-ufpm_expiration_date',)
--- asts_fitness_program/unit/models.py
+++ asts_fitness_program/unit/models.py
@@ -1,26 +1,49 @@
 from django.db import models
 
 
-class Fitness_Level(models.Model):
-    fitness_level_id = models.AutoField(primary_key=True, serialize=True)
-    fitness_level = models.CharField(max_length=50)
-
-    def __str__(self):
-        return f"{self.fitness_level}"
-
-
 class Airman(models.Model):
+    RANK_CHOICE = (
+        ('AB', 'AB'),
+        ('AMN', 'Amn'),
+        ('A1C', 'A1C'),
+        ('SRA', 'SrA'),
+        ('SSGT', 'SSgt'),
+        ('TSGT', 'TSgt'),
+        ('MSGT', 'MSgt'),
+        ('SMSGT', 'SMSgt'),
+        ('CMSGT', 'CMSgt'),
+        ('2ND LT', '2nd Lt'),
+        ('1ST LT', '1st Lt'),
+        ('CAPT', 'Capt'),
+        ('MAJ', 'Maj'),
+        ('LT COL', 'Lt Col'),
+        ('COL', 'Col'),
+        ('BRIG GEN', 'Brig Gen'),
+        ('MAJ GEN', 'Maj Gen'),
+        ('LT GEN', 'Lt Gen'),
+        ('GEN', 'General'),
+    )
+    FITNESS_CHOICE = (
+        ('excellent', 'EXCELLENT'),
+        ('satisfactory', 'SATISFACTORY'),
+        ('pass', 'PASS'),
+        ('unsatisfactory', 'UNSATISFACTORY'),
+        ('fail', 'FAIL'),
+        ('exemption', 'EXEMPTION'),
+    )
     airman_id = models.AutoField(primary_key=True, serialize=True)
-    fitness_level_id = models.ForeignKey(
-        Fitness_Level,
-        on_delete=models.DO_NOTHING, )
-    rank = models.CharField(max_length=10)
+    fitness_level = models.CharField(max_length=50,
+                                     choices=FITNESS_CHOICE,
+                                     default='satisfactory')
+    rank = models.CharField(max_length=10,
+                            choices=RANK_CHOICE,
+                            default='ab')
     first_name = models.CharField(max_length=50)
     middle_initial = models.CharField(max_length=5)
     last_name = models.CharField(max_length=50)
     ssn = models.IntegerField()
-    slug = models.SlugField(max_length=9,
-                            unique_for_date='test_date')
+    airman_slug = models.SlugField(max_length=9,
+                                   unique_for_date='test_date')
     test_date = models.DateField()
     ptl = models.BooleanField(default=False)
     ufpm = models.BooleanField(default=False)
@@ -47,12 +70,12 @@ class Physical_Training_Leader(models.Model):
     airman_id = models.ForeignKey(
         Airman,
         on_delete=models.DO_NOTHING, )
-    certification_date = models.DateField()
+    ptl_certification_date = models.DateField()
     ptl_expiration_date = models.DateField()
     cpr_expiration_date = models.DateField()
 
     def __str__(self):
-        return f"{self.airman_id}: Certified on {self.certification_date} and expires on {self.ptl_expiration_date}"
+        return f"{self.airman_id}: Certified on {self.ptl_certification_date} and expires on {self.ptl_expiration_date}"
 
 
 class Unit_Fitness_Program_Manager(models.Model):
@@ -80,4 +103,4 @@ class Profile(models.Model):
     profile_details = models.TextField()
 
     def __str__(self):
-        return f"{self.airman_id}:\nActivities Allowed: {self.activities_allowed}\nExpiration Date: {self.profile_expiration_date}"
+        return f"{self.airman_id}: Profile Expiration Date: {self.profile_expiration_date}"
